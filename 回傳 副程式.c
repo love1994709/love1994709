4c8c1b5eea4c2ea4a2a0b7321b18e2eccdcecee1
love1994709 <love1994709@gmail.com>
2020-05-16 18:52:08 +0800

    回傳 副程式

---
+++ 回傳 副程式.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+int main()
+ {
+ 	ex01();
+	int i = ex02(10);
+	printf("%d",i);
+	system("pause");
+	  
+	return 0;
+ 	
+ }
+ void ex01()
+ {
+ 	printf("我是副程式");
+	  
+ }
+  int ex02(int a)
+  {
+  	return a;
+  	
+  	
+  }
